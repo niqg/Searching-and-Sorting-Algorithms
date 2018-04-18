0687958f03e7f0deb3eaafaf6d4e4a484e4eaffe
niqg <32465777+niqg@users.noreply.github.com>
2018-04-18 14:50:24 -0400

    Update ImprovedInsertionSort.java

--- Sorting Algorithms/Insertion Sort/ImprovedInsertionSort.java
+++ Sorting Algorithms/Insertion Sort/ImprovedInsertionSort.java
@@ -1,5 +1,5 @@
 public static void improvedInsertionSort(Integer [] array)
-        {
+{
         int size=array.length;
         int swaps=0;
         int comparisons=0;
@@ -8,47 +8,39 @@ public static void improvedInsertionSort(Integer [] array)
 
         while(size>source)
         {
+                int start=0;
+                int end=source-1;
+                int midpoint=end/2;
+                temp=array[source];
 
-        int start=0;
-        int end=source-1;
-        int midpoint=end/2;
-        temp=array[source];
+                while(end>start)
+                {
+                        midpoint=(end+start)/2;
+                        comparisons++;
 
-        while(end>start)
-        {
+                        if(array[source]<array[midpoint])
+                        {
+                                end=midpoint;
+                        }
+                        else
+                        {
+                                start=midpoint+1;
+                        }
+                }
 
-        midpoint=(end+start)/2;
-        comparisons++;
+                for(int i=source;i>midpoint;i--)
+                {
 
-        if(array[source]<array[midpoint])
-        {
+                array[i]=array[i-1];
+                swaps++;
 
-        end=midpoint;
+                }
 
-        }
-        else
-        {
-
-        start=midpoint+1;
-
-        }
-        }
-
-        for(int i=source;i>midpoint;i--)
-        {
-
-        array[i]=array[i-1];
-        swaps++;
-
-        }
-
-        array[midpoint]=temp;
-        source++;
-
+                array[midpoint]=temp;
+                source++;
         }
 
         System.out.println("# of swaps: "+swaps+" # of Comparisons: "+comparisons+"\n");
-
-        }
+}
 
 
