b947e689dd61751a919857224f9e6424f4364267
niqg <32465777+niqg@users.noreply.github.com>
2018-04-18 14:51:05 -0400

    Update InsertionSort.java

--- Sorting Algorithms/Insertion Sort/InsertionSort.java
+++ Sorting Algorithms/Insertion Sort/InsertionSort.java
@@ -1,6 +1,5 @@
 public static void insertionSort(Integer [] array)
-        {
-
+{
         int size = array.length;
         int swaps = 0;
         int comparisons = 0;
@@ -8,29 +7,22 @@ public static void insertionSort(Integer [] array)
 
         while(size > source)
         {
-
             for(int i = 0; i < source ; i++ , comparisons++)
             {
-
                 if(array[i] > array[source])
                 {
-
                     for(int j = source ; j > i ; j--)
                     {
-
                         int temp = array[j-1];
                         array[j-1] = array[j];
                         array[j] = temp;
                         swaps++;
-
                     }
                 }
             }
 
             source++;
-
         }
 
         System.out.println("# of swaps: " + swaps + " # of Comparisons: " + comparisons + "\n");
-
-        }
+}
