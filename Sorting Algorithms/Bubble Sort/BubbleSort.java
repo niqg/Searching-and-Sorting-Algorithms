0f59187139de78ea73fff3447db54e101aafdd74
niqg <32465777+niqg@users.noreply.github.com>
2018-04-18 14:39:10 -0400

    Update BubbleSort.java

--- Sorting Algorithms/Bubble Sort/BubbleSort.java
+++ Sorting Algorithms/Bubble Sort/BubbleSort.java
@@ -1,30 +1,26 @@
 public static void bubbleSort(Integer [] array)
+{
+        int size = array.length;
+        int swaps = 0;
+        int comparisons = 0;
+
+        while(size > 1)
         {
-            int size = array.length;
-            int swaps = 0;
-            int comparisons = 0;
-
-
-            while(size > 1)
-            {
-
                 for(int i = 0 , j = 1 ; j < size ; i++ , j++, comparisons++)
                 {
 
-                    if(array[i] > array[j])
-                    {
-
-                    swaps++;
-                    Integer temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
-
-                    }
+                        if(array[i] > array[j])
+                        {
+                                swaps++;
+                                Integer temp = array[j];
+                                array[j] = array[i];
+                                array[i] = temp;
+                        }
                 }
 
                 size--;
 
-            }
-
-            System.out.println("# of swaps: " + swaps + " # of Comparisons: " + comparisons + "\n");
         }
+
+        System.out.println("# of swaps: " + swaps + " # of Comparisons: " + comparisons + "\n");
+}
